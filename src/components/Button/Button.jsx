8b5c258b502db8a1ba63bb4b15934f1b6d7fbc69
aditya-9645 <adityab201999@gmail.com>
2025-04-13 23:41:52 +0530

    Completed Project

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,8 +1,8 @@
 import styles from './Button.module.css';
 
-const Button = ({isOutline, text, icon}) => {
+const Button = ({isOutline, text, icon, type = "button"}) => {
   return (
-    <button className={ isOutline? styles.outlineBtn : styles.primaryBtn }>
+    <button type={type} className={ isOutline? styles.outlineBtn : styles.primaryBtn }>
         {icon}
         {text}
     </button>
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -11,10 +11,25 @@ const ContactForm = () => {
     <section className={`${styles.contactSection} container`}>
         <div className={`${styles.contactForm}`}>
           <div className={`${styles.twoPrimaryBtns}`}>
-            <Button text="Via Support Chat" icon={<MdMessage fontSize="24px"/>}/>
-            <Button text="Via Call" icon={<FaPhoneAlt  fontSize="24px"/>}/> 
+            <Button text="VIA SUPPORT CHAT" icon={<MdMessage fontSize="24px"/>}/>
+            <Button text="VIA CALL" icon={<FaPhoneAlt  fontSize="24px"/>}/> 
           </div>
-          <Button isOutline = {true} text="Via Email Form" icon={<MdEmail  fontSize="24px"/>}/>
+          <Button isOutline = {true} text="VIA EMAIL FORM" icon={<MdEmail  fontSize="24px"/>}/>
+          <form>
+              <div className={styles.form_control}>
+                <label htmlFor="name">Name</label>
+                <input type="text" name="name"/>
+              </div>
+              <div className={styles.form_control}>
+                <label htmlFor="email">E-mail</label>
+                <input type="email" name="email"/>
+              </div>
+              <div className={styles.form_control}>
+                <label htmlFor="text">Query or Feedback</label>
+                <textarea name="text"/>
+              </div>
+              <Button isOutline = {true} text="SUBMIT" type="submit" icon={<MdEmail  fontSize="24px"/>}/>
+          </form>
         </div>
         <div className={`${styles.contactImage}`}>
           <img src="/images/contact_image.png" alt="miscallenous_image" />
